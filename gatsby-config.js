97797c544db5448ddbfa70732cf8788bd70cd000
rajkothari634 <54576688+rajkothari634@users.noreply.github.com>
2021-08-13 13:37:35 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,3 +9,13 @@ module.exports = {
   pathPrefix: "/portfolio",
   plugins: [],
 }
+
+const config = {
+  siteMetadata: {
+    title: `Raj Kotahri Portfolio`,
+    description: `Full Stack Developer | Machine Learning | MongoDb | Tech Writer | Mentor | Mobile Dev | Community Builder | Learner`,
+    author: `Raj Kothari`,
+    siteUrl: `https://bit.ly/rajkothari634`,
+    imageUrl: `https://i.ibb.co/LP8cqdd/rkimage.jpg`
+  },
+}
